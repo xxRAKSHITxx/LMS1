7e36e2f51f168523a0fd6f69cb269e6aa3892970
Rakshit <rakshitmudgal@gmail.com>
2025-01-12 01:02:24 +0530

    changes14

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'));
 const corsOptions = {
   origin: function (origin, callback) {
     const allowedOrigins = [
-      'https://udyamshala.com/', 
+      'https://udyamshala.com', 
       'http://localhost:5173',  // Local development
       'https://your-production-domain.vercel.app', // Add your exact production domain
       'https://lms-1-34wr5ym84-xxrakshitxxs-projects.vercel.app/'
