7d8568266f43ae191e40ced19c93c08a83e8c5ef
Rakshit <rakshitmudgal@gmail.com>
2025-01-21 02:10:40 +0530

    major5

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -2,11 +2,7 @@ import axios from 'axios';
 
 const BASE_URL = import.meta.env.VITE_REACT_APP_API_URL;
 
-const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_REACT_APP_API_URL,
-    withCredentials: true,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    }
-  });
+export const axiosInstance = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true
+})
